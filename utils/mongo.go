71644fb2d0fac8c02db94a18c27613e29909239c
wuxu92 <wuxu92@163.com>
2016-01-21 11:10:57 +0800

    new format

--- utils/mongo.go
+++ utils/mongo.go
@@ -1,48 +1,53 @@
 package utils
 
 import (
-  "gopkg.in/mgo.v2"
-//  "gopkg.in/mgo.v2/bson"
-  "github.com/astaxie/beego"
-//  "strings"
+	"gopkg.in/mgo.v2"
+	//  "gopkg.in/mgo.v2/bson"
+	"github.com/astaxie/beego"
+	//  "strings"
 )
 
 type Mgc struct {
-  session *mgo.Session
+	session *mgo.Session
 }
 
-func (mgc *Mgc) GetSession() *mgo.Session{
-  if mgc.session == nil {
-    host := beego.AppConfig.String("mgoHost")
-    port := beego.AppConfig.String("port")
-    if len(host) == 0 {
-      host = "127.0.0.1"
-    }
-    if len(port) > 0 {
-      host += ":"+port
-    }
-    session, err := mgo.Dial(host)
-    ErrChk(err)
-    mgc.session = session
-    mgc.session.SetMode(mgo.Monotonic, true)
-  }
-  return mgc.session
+func (mgc *Mgc) GetSession() *mgo.Session {
+	if mgc.session == nil {
+		host := beego.AppConfig.String("mgoHost")
+		port := beego.AppConfig.String("port")
+		user := beego.AppConfig.String("mgoUser")
+		password := beego.AppConfig.String("mgoPassword")
+		if len(host) == 0 {
+			host = "127.0.0.1"
+		}
+		if len(port) > 0 {
+			host += ":" + port
+		}
+		if len(user) > 0 {
+			host = "mongodb://" + user + ":" + password + "@" + host
+		}
+		session, err := mgo.Dial(host)
+		ErrChk(err)
+		mgc.session = session
+		mgc.session.SetMode(mgo.Monotonic, true)
+	}
+	return mgc.session
 }
 
-func (this *Mgc) GetDB() *mgo.Database{
-  dbName := beego.AppConfig.String("mgoDB")
-  if len(dbName) == 0 {
-    dbName = "snp"
-  }
-  db := this.GetSession().DB(dbName)
-  return db
+func (this *Mgc) GetDB() *mgo.Database {
+	dbName := beego.AppConfig.String("mgoDB")
+	if len(dbName) == 0 {
+		dbName = "snp"
+	}
+	db := this.GetSession().DB(dbName)
+	return db
 }
 
 func (this *Mgc) Close() {
-  this.session.Close()
+	this.session.Close()
 }
 
 func GetDBStatic() *mgo.Database {
-  mgc := Mgc{}
-  return mgc.GetDB()
+	mgc := Mgc{}
+	return mgc.GetDB()
 }
